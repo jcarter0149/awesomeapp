d8db28350df063577c37de30c545c51c0195a0b6
jcarter0149 <jacobraycarter@gmail.com>
2018-04-26 14:39:39 -0500

    added modules

--- src/scripts/awesome.js
+++ src/scripts/awesome.js
@@ -6,9 +6,9 @@
     5. Clearing fields
 */
 const DomBuilder = require("./DOMbuilder")
-
-
-
+const CardFactory = require("./cardFactory")
+const ButtonFactory = require("./buttonFactory")
+const InputFieldFactory = require ("./inputFactory")
 
 
 
@@ -19,48 +19,19 @@ const output = document.querySelector(".output")
 // Fragment to hold all the stuff so we inject into the DOM once
 const fragment = document.createDocumentFragment()
 
-// Create factory function to generate components
-// Create input component
-const inputFieldFactory = (classList, defaultPlaceholderText, type) => {
-    const inputField = document.createElement("input")
-    inputField.setAttribute("type", type)
-    inputField.classList = classList
-    inputField.placeholder = defaultPlaceholderText
-    return inputField
-}
-
-
-// Create button component
-const buttonFactory = (classList, textContent) => {
-    const theButton = document.createElement("button")
-    theButton.classList = classList
-    theButton.textContent = textContent
-    return theButton
-}
-
-// Create card component
-const cardFactory = (classList, textContent) => {
-    const theSection = document.createElement("section")
-    theSection.classList = classList
-    theSection.textContent = textContent
-    return theSection
-}
-
-const createCardButton = buttonFactory("button--submit", "Create Card")
-const cardTextInput = inputFieldFactory("input--text", "Enter card text here", "text")
-
-/*
-    Attach event listener to button
-*/
-createCardButton.addEventListener("click", function () {
+const createCardButton = ButtonFactory("button--submit", "Create Card", function () {
     // 1. Get value of input field
     const userEntry = cardTextInput.value
 
     // 2. Create card component with text inside
-    output.appendChild(cardFactory("card", userEntry))
+    output.appendChild(CardFactory("card", userEntry))
 
     cardTextInput.value = ""
 })
+
+
+const cardTextInput = InputFieldFactory("input--text", "Enter card text here", "text")
+
 
 fragment.appendChild(cardTextInput)
 fragment.appendChild(createCardButton)
